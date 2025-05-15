2cf28f9d25d7f61d83d498346fc22e689b07f448
Patryk10245 <Patryk10245@190313-KOMPUTER>
2025-05-15 22:09:49 +0200

    Dodaj pliki projektów.

---
+++ Form1.cs
@@ -0,0 +1,18 @@
+namespace Projekt_ZPO
+{
+
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+    
